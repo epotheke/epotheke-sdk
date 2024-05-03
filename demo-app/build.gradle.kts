6f3c0cac72323c4dc5474fe77fd74ba0fba751b5
Florian Otto <florian.otto@ecsec.de>
2024-05-03 12:19:43 +0200

    fix: Exclude META-INF files in android app

--- demo-app/build.gradle.kts
+++ demo-app/build.gradle.kts
@@ -62,6 +62,8 @@ android {
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            excludes += "/META-INF/LICENSE.md"
+            excludes += "/META-INF/NOTICE.md"
         }
     }
     buildTypes {
