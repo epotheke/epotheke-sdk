21821b90372b2d31021b1691a57cf9d0a1146372
Florian Otto <florian.otto@ecsec.de>
2024-06-25 14:03:01 +0200

    chore: add header comment

--- sdk/src/androidMain/kotlin/com/epotheke/sdk/ErezeptProtocolImp.android.kt
+++ sdk/src/androidMain/kotlin/com/epotheke/sdk/ErezeptProtocolImp.android.kt
@@ -1,3 +1,25 @@
+/****************************************************************************
+ * Copyright (C) 2024 ecsec GmbH.
+ * All rights reserved.
+ * Contact: ecsec GmbH (info@ecsec.de)
+ *
+ * This file is part of the epotheke SDK.
+ *
+ * GNU General Public License Usage
+ * This file may be used under the terms of the GNU General Public
+ * License version 3.0 as published by the Free Software Foundation
+ * and appearing in the file LICENSE.GPL included in the packaging of
+ * this file. Please review the following information to ensure the
+ * GNU General Public License version 3.0 requirements will be met:
+ * http://www.gnu.org/copyleft/gpl.html.
+ *
+ * Other Usage
+ * Alternatively, this file may be used in accordance with the terms
+ * and conditions contained in a signed written agreement between
+ * you and ecsec GmbH.
+ *
+ ***************************************************************************/
+
 package com.epotheke.sdk
 
 import android.annotation.TargetApi
@@ -10,7 +32,6 @@ import kotlinx.coroutines.withTimeout
 import kotlinx.serialization.encodeToString
 import java.time.LocalDateTime.now
 import java.util.concurrent.TimeoutException
-import kotlin.time.Duration
 import kotlin.time.Duration.Companion.seconds
 import kotlin.time.toKotlinDuration
 
