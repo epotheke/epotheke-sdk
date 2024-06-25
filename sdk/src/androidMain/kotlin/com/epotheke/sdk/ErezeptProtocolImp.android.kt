44e60f1b064d2f37fae7748de9b7480e1bfa9b97
Florian Otto <florian.otto@ecsec.de>
2024-06-25 13:38:32 +0200

    feat: Add timeout in Ereceiptprotocol implementation

--- sdk/src/androidMain/kotlin/com/epotheke/sdk/ErezeptProtocolImp.android.kt
+++ sdk/src/androidMain/kotlin/com/epotheke/sdk/ErezeptProtocolImp.android.kt
@@ -2,17 +2,22 @@ package com.epotheke.sdk
 
 import com.epotheke.erezept.model.*
 import io.github.oshai.kotlinlogging.KotlinLogging
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.withTimeout
 import kotlinx.serialization.encodeToString
+import java.util.concurrent.TimeoutException
+import kotlin.time.Duration
 
 private val logger = KotlinLogging.logger {}
+private val ReceiveTimeout = Duration.parse("30s")
 
 class ErezeptProtocolImp(
     private val ws: WebsocketAndroid,
 ) : ErezeptProtocol {
 
     private val inputChannel = Channel<String>()
-
+    @OptIn(ExperimentalCoroutinesApi::class)
     override suspend fun requestReceipts(req: RequestPrescriptionList): AvailablePrescriptionLists {
         logger.debug { "Sending data to request ecreipts." }
 
@@ -22,7 +27,11 @@ class ErezeptProtocolImp(
 
         while (true) {
             try {
-                val response = inputChannel.receive()
+                val response = withTimeout(
+                    timeout = ReceiveTimeout
+                ){
+                    inputChannel.receive()
+                }
                 when (val eRezeptMessage = eRezeptJsonFormatter.decodeFromString<ERezeptMessage>(response)) {
                     is AvailablePrescriptionLists -> {
                         if (eRezeptMessage.correlationId == req.messageId) {
@@ -35,6 +44,16 @@ class ErezeptProtocolImp(
                     else -> continue
                 }
             } catch (e: Exception) {
+                when (e) {
+                    is TimeoutException -> {
+                        logger.error { "Timeout during receive" }
+                        throw ErezeptProtocolException(GenericErrorMessage(
+                            errorCode = GenericErrorResultType.UNKNOWN_ERROR,
+                            errorMessage = "Timeout",
+                            correlationId = req.messageId
+                        ))
+                    }
+                }
                 logger.error(e) { "Exceptino during receive" }
                 throw e
             }
@@ -48,7 +67,11 @@ class ErezeptProtocolImp(
         )
         while (true) {
             try {
-                val response = inputChannel.receive()
+                val response = withTimeout(
+                    timeout = ReceiveTimeout
+                ){
+                    inputChannel.receive()
+                }
                 when (val eRezeptMessage = eRezeptJsonFormatter.decodeFromString<ERezeptMessage>(response)) {
                     is ConfirmPrescriptionList -> {
                         if (eRezeptMessage.correlationId == lst.messageId) {
@@ -61,6 +84,16 @@ class ErezeptProtocolImp(
                     else -> continue
                 }
             } catch (e: Exception) {
+                when (e) {
+                    is TimeoutException -> {
+                        logger.error { "Timeout during receive" }
+                        throw ErezeptProtocolException(GenericErrorMessage(
+                            errorCode = GenericErrorResultType.UNKNOWN_ERROR,
+                            errorMessage = "Timeout",
+                            correlationId = lst.messageId
+                        ))
+                    }
+                }
                 logger.error(e) { "Exception during receive" }
                 throw e
             }
