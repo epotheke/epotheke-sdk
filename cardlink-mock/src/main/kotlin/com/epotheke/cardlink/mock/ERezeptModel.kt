be2d6445f5109978ebc3b372008e75d36e59d772
Mike Prechtl <mike.prechtl@ecsec.de>
2024-07-03 09:49:13 +0200

    feat: Update eRezept model

--- cardlink-mock/src/main/kotlin/com/epotheke/cardlink/mock/ERezeptModel.kt
+++ cardlink-mock/src/main/kotlin/com/epotheke/cardlink/mock/ERezeptModel.kt
@@ -20,9 +20,18 @@ const val MEDICATION_FREE_TEXT = "medicationFreeText"
 const val MEDICATION_COMPOUNDING = "medicationCompounding"
 const val MEDICATION_INGREDIENT = "medicationIngredient"
 const val BESTANDTEIL_WIRKSTOFF_VERORDNUNG= "bestandteilWirkstoffverordnung"
-const val MEDICATION_SUMMARY = "medicationSummary"
-const val CONFIRM_PRESCRIPTION_LIST = "confirmPrescriptionListMessage"
-
+const val BESTANDTEIL_REZEPTUR_VERORDNUNG = "bestandteilRezepturverordnung"
+const val SELECTED_PRESCRIPTION_LIST_RESPONSE = "selectedPrescriptionListResponse"
+const val PRESCRIPTION_BUNDLE = "prescriptionBundle"
+const val MEDICATION = "medication"
+const val ORGANIZATION = "organization"
+const val PATIENT = "patient"
+const val PERSON = "person"
+const val STREET_ADDRESS = "streetAddress"
+const val POB_ADDRESS = "pobAddress"
+const val PRACTICE_SUPPLY = "practiceSupply"
+const val PRACTITIONER = "practitioner"
+const val PRESCRIPTION = "prescription"
 
 
 sealed interface ERezeptMessage
@@ -48,12 +57,15 @@ data class AvailablePrescriptionLists(
 data class SelectedPrescriptionList(
     @SerialName("ICCSN")
     val iccsn: ByteArrayAsBase64,
-    val medicationIndexList: List<Int>,
+    val prescriptionIndexList: List<String>,
+    val version: String? = null,
     val supplyOptionsType: SupplyOptionsType,
     val name: String? = null,
-    val address: List<String>? = null,
+    val address: StreetAddress? = null,
     val hint: String? = null,
+    val text: String? = null,
     val phone: String? = null,
+    val mail: String? = null,
     val messageId: String = UUID.randomUUID().toString(),
 ) : ERezeptMessage
 
@@ -67,9 +79,16 @@ data class GenericErrorMessage(
 ) : ERezeptMessage
 
 @Serializable
-@SerialName(CONFIRM_PRESCRIPTION_LIST)
-data class ConfirmPrescriptionList(
-    val messageId: String = UUID.randomUUID().toString(),
+@SerialName(SELECTED_PRESCRIPTION_LIST_RESPONSE)
+data class SelectedPrescriptionListResponse(
+    val type: String,
+    val version: String? = null,
+    val supplyOptionsType: SupplyOptionsType? = null,
+    val infoText: String? = null,
+    val url: String? = null,
+    val pickUpCodeHr: String? = null,
+    val pickUpCodeDmc: String? = null,
+    val messageId: String,
     val correlationId: String,
 ) : ERezeptMessage
 
@@ -78,8 +97,23 @@ data class ConfirmPrescriptionList(
 data class AvailablePrescriptionList(
     @SerialName("ICCSN")
     val iccsn: ByteArrayAsBase64,
-    val medicationSummaryList: List<MedicationSummary>,
-    val hint: String? = null,
+    val prescriptionBundleList: List<PrescriptionBundle>,
+)
+
+@Serializable
+@SerialName(PRESCRIPTION_BUNDLE)
+data class PrescriptionBundle(
+    val prescriptionid: String,
+    val erstellungszeitpunkt: String,
+    val status: String,
+    val pkvTarif: String? = null,
+    val patient: Patient,
+    val arzt: Practitioner,
+    val pruefnummer: String? = null,
+    val organisation: Organization,
+    val verordnung: PrescriptionInterface,
+    val asvTn: String? = null,
+    val arzneimittel: Medication,
 )
 
 @Serializable
@@ -89,6 +123,17 @@ data class BestandteilWirkstoffverordnung(
     val wirkstoffname: String,
     val wirkstaerke: String,
     val wirkstaerkeneinheit: String,
+)
+
+@Serializable
+@SerialName(BESTANDTEIL_REZEPTUR_VERORDNUNG)
+data class BestandteilRezepturverordnung(
+    val darreichungsform: String,
+    val name: String,
+    val pzn: String? = null,
+    val menge: String? = null,
+    val einheit: String? = null,
+    val mengeUndEinheit: String? = null,
 )
 
 enum class GenericErrorResultType(val value: String) {
@@ -102,7 +147,7 @@ enum class GenericErrorResultType(val value: String) {
     UNKNOWN_ERROR("UNKNOWN_ERROR");
 }
 
-sealed interface Medication
+sealed interface MedicationItem
 
 @Serializable
 @SerialName(MEDICATION_COMPOUNDING)
@@ -115,8 +160,8 @@ data class MedicationCompounding(
     val darreichungsform: String,
     val gesamtmenge: String,
     val einheit: String? = null,
-    val listeBestandteilRezepturverordnung: List<BestandteilWirkstoffverordnung>,
-) : Medication
+    val listeBestandteilRezepturverordnung: List<BestandteilRezepturverordnung>,
+) : MedicationItem
 
 @Serializable
 @SerialName(MEDICATION_FREE_TEXT)
@@ -125,7 +170,7 @@ data class MedicationFreeText(
     val impfstoff: Boolean,
     val freitextverordnung: String,
     val darreichungsform: String,
-) : Medication
+) : MedicationItem
 
 @Serializable
 @SerialName(MEDICATION_INGREDIENT)
@@ -138,7 +183,7 @@ data class MedicationIngredient(
     val einheit: String? = null,
     val packungsgroesseNachNBezeichnung: String? = null,
     val listeBestandteilWirkstoffverordnung: List<BestandteilWirkstoffverordnung>,
-) : Medication
+) : MedicationItem
 
 @Serializable
 @SerialName(MEDICATION_PZN)
@@ -152,13 +197,118 @@ data class MedicationPzn(
     val packungsgroesseNachMenge: String? = null,
     val einheit: String? = null,
     val packungsgroesseNachNBezeichnung: String? = null,
-) : Medication
+) : MedicationItem
 
 @Serializable
-@SerialName(MEDICATION_SUMMARY)
-data class MedicationSummary(
-    val index: Int,
-    val medication: Medication,
+@SerialName(MEDICATION)
+data class Medication(
+    val medicationItem: List<MedicationItem>,
+)
+
+@Serializable
+@SerialName(ORGANIZATION)
+data class Organization(
+    val bsnr: String? = null,
+    val ikNummer: String? = null,
+    val kzvAn: String? = null,
+    val standortnummer: String? = null,
+    val telematikId: String? = null,
+    val name: String? = null,
+    val address: StreetAddress? = null,
+    val telefon: String? = null,
+    val fax: String? = null,
+    val eMail: String? = null,
+)
+
+@Serializable
+@SerialName(PATIENT)
+data class Patient(
+    val gkvVersichertenId: String? = null,
+    val pkvVersichertenId: String? = null,
+    val kvkVersichertennummer: String? = null,
+    val person: Person,
+    val geburtsdatum: String,
+    val adresse: Address? = null,
+)
+
+@Serializable
+@SerialName(PERSON)
+data class Person(
+    val vorname: String,
+    val name: String? = null,
+    val titel: String? = null,
+    val namenszusatz: String? = null,
+    val vorsatzwort: String? = null,
+)
+
+sealed interface Address
+
+@Serializable
+@SerialName(STREET_ADDRESS)
+data class StreetAddress(
+    val land: String? = null,
+    val plz: String,
+    val ort: String,
+    val strasse: String,
+    val hausnummer: String,
+    val zusatz: String? = null,
+) : Address
+
+@Serializable
+@SerialName(POB_ADDRESS)
+data class PobAddress(
+    val land: String? = null,
+    val plz: String,
+    val ort: String,
+    val postfach: String,
+) : Address
+
+sealed interface PrescriptionInterface
+
+@Serializable
+@SerialName(PRACTICE_SUPPLY)
+data class PracticeSupply(
+    val datum: String,
+    val anzahl: Int,
+    val kostentraegertyp: String,
+    val ikNummer: String,
+    val name: String,
+) : PrescriptionInterface
+
+@Serializable
+@SerialName(PRESCRIPTION)
+data class Prescription(
+    val ausstellungsdatum: String? = null,
+    val noctu: Boolean? = null,
+    val bvg: Boolean? = null,
+    val zuzahlungsstatus: String? = null,
+    val autidem: Boolean? = null,
+    val anzahl: Int? = null,
+    val dosierung: Boolean? = null,
+    val dosieranweisung: String? = null,
+    val gebrauchsanweisung: String? = null,
+    val unfallkennzeichen: String? = null,
+    val unfalltag: String? = null,
+    val unfallbetrieb: String? = null,
+    val mehrfachverordnung: Boolean? = null,
+    val mfvId: String? = null,
+    val mfvZaehler: Int? = null,
+    val mfvNenner: Int? = null,
+    val mfvBeginn: String? = null,
+    val mfvEnde: String? = null,
+) : PrescriptionInterface
+
+@Serializable
+@SerialName(PRACTITIONER)
+data class Practitioner(
+    val typ: String,
+    val berufsbezeichnung: String? = null,
+    val asvFgn: String? = null,
+    val arztnummer: String? = null,
+    val zahnarztnummer: String? = null,
+    val telematikId: String? = null,
+    val person: Person,
+    val verantwortlichePerson: Practitioner? = null,
 )
 
 enum class SupplyOptionsType {
@@ -175,14 +325,22 @@ val eRezeptModule = SerializersModule {
         subclass(RequestPrescriptionList::class)
         subclass(AvailablePrescriptionLists::class)
         subclass(SelectedPrescriptionList::class)
-        subclass(ConfirmPrescriptionList::class)
+        subclass(SelectedPrescriptionListResponse::class)
         subclass(GenericErrorMessage::class)
     }
-    polymorphic(Medication::class) {
+    polymorphic(MedicationItem::class) {
         subclass(MedicationCompounding::class)
         subclass(MedicationIngredient::class)
         subclass(MedicationFreeText::class)
         subclass(MedicationPzn::class)
+    }
+    polymorphic(PrescriptionInterface::class) {
+        subclass(PracticeSupply::class)
+        subclass(Prescription::class)
+    }
+    polymorphic(Address::class) {
+        subclass(StreetAddress::class)
+        subclass(PobAddress::class)
     }
 }
 
