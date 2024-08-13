fed605ecc4228a45f2baabdedb4844c11f4239e5
Florian Otto <florianotto@macairM1.fritz.box>
2024-08-13 11:15:17 +0200

    feat: Add new ios-demo-app

---
+++ demo-ios-standalone/EpothekeDemo/EpothekeDemo/EpothekeDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  EpothekeDemoApp.swift
+//  EpothekeDemo
+//
+//  Created by Florian Otto on 13.08.24.
+//
+
+import SwiftUI
+
+@main
+struct EpothekeDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
