07ee58d519f1dd524531879e8ec69dfb27f150a8
Florian Otto <florian.otto@ecsec.de>
2024-10-22 07:42:34 +0200

    chore: inc sdk version 1.1.11-snap android demo

--- demo-android-standalone/app/build.gradle.kts
+++ demo-android-standalone/app/build.gradle.kts
@@ -44,7 +44,7 @@ android {
     }
 }
 
-val epothekeSdkVersion = "1.1.10"
+val epothekeSdkVersion = "1.1.11-SNAPSHOT"
 
 dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
