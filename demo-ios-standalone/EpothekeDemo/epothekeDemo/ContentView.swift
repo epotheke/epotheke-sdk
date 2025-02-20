9c8225bbd248e71780b0cade5b3385c66824e7f9
Florian Otto <florianotto@macairM1.fritz.box>
2025-02-20 15:25:41 +0100

    feat: Update demo-ios-standalone regarding error handling

--- demo-ios-standalone/EpothekeDemo/epothekeDemo/ContentView.swift
+++ demo-ios-standalone/EpothekeDemo/epothekeDemo/ContentView.swift
@@ -5,10 +5,16 @@
 //  Created by Florian Otto on 13.08.24.
 //
 
-import SwiftUI
 import CoreNFC
 import OpenEcard.open_ecard_mobile_lib
+import SwiftUI
 import epotheke
+
+extension NSError {
+    func toGenericErrorMessage() -> GenericErrorMessage? {
+        return (self.userInfo["KotlinException"] as? PrescriptionProtocolException)?.msg
+    }
+}
 
 struct ContentView: View {
     @State var showCanAlert = false
@@ -19,7 +25,7 @@ struct ContentView: View {
     @State var nbr = "+49 22211133"
 
     @State var cb = {}
-    
+
     var body: some View {
         VStack {
             Button {
@@ -27,22 +33,22 @@ struct ContentView: View {
                 performEpo()
             } label: {
                 Text("epotheke Demo")
-            } .alert(Text("Phonenumber"), isPresented: self.$showNumberAlert){
-                Button("OK"){
+            }.alert(Text("Phonenumber"), isPresented: self.$showNumberAlert) {
+                Button("OK") {
                     self.$cb.wrappedValue()
                 }
                 TextField("nbr", text: $nbr).textContentType(.telephoneNumber)
-            } message : {
+            } message: {
                 Text("Enter Phonenumber")
-            }.alert(Text("TAN"), isPresented: self.$showTanAlert){
-                Button("OK"){
+            }.alert(Text("TAN"), isPresented: self.$showTanAlert) {
+                Button("OK") {
                     self.$cb.wrappedValue()
                 }
                 TextField("tan", text: $tan).textContentType(.oneTimeCode)
-            } message : {
+            } message: {
                 Text("Enter TAN")
-            }.alert(Text("CAN"), isPresented: self.$showCanAlert){
-                Button("OK"){
+            }.alert(Text("CAN"), isPresented: self.$showCanAlert) {
+                Button("OK") {
                     self.$cb.wrappedValue()
                 }
                 TextField("can", text: $can).textContentType(.oneTimeCode)
@@ -83,11 +89,10 @@ struct ContentView: View {
             return "Connected with the card."
         }
 
-
     }
 
     /* This implementation will handle errors whcih might occur within sdk or during processes the sdk handles.*/
-    class SdkErrorHandlerImp :NSObject, SdkErrorHandler {
+    class SdkErrorHandlerImp: NSObject, SdkErrorHandler {
         func hdl(code: String, error: String) {
             print("error code:" + code)
             print("error :" + error)
@@ -99,14 +104,13 @@ struct ContentView: View {
      Within the app the user can get asked for the CAN.
      After that the handed in callback handler has to be called with the given value to resume the process.
      */
-    class CardLinkInteraction: NSObject,  CardLinkInteractionProtocol{
-        var v :ContentView
+    class CardLinkInteraction: NSObject, CardLinkInteractionProtocol {
+        var v: ContentView
 
-        init(v:ContentView) {
+        init(v: ContentView) {
             self.v = v
             super.init()
         }
-
 
         func onCanRequest(_ enterCan: (NSObjectProtocol & ConfirmPasswordOperationProtocol)!) {
             print("onCanRequest")
@@ -116,7 +120,7 @@ struct ContentView: View {
             }
             self.v.showCanAlert = true
         }
-	func onCanRetry(
+        func onCanRetry(
             _ enterCan: (any NSObjectProtocol & ConfirmPasswordOperationProtocol)!,
             withResultCode resultCode: String!,
             withErrorMessage errorMessage: String!
@@ -129,8 +133,8 @@ struct ContentView: View {
             }
             self.v.showCanAlert = true
 
-       }
-       func onPhoneNumberRequest(_ enterPhoneNumber: (NSObjectProtocol & ConfirmTextOperationProtocol)!) {
+        }
+        func onPhoneNumberRequest(_ enterPhoneNumber: (NSObjectProtocol & ConfirmTextOperationProtocol)!) {
             print("onPhoneNumberRequest")
             self.v.cb = {
                 enterPhoneNumber.confirmText(self.v.$nbr.wrappedValue)
@@ -143,7 +147,7 @@ struct ContentView: View {
             withErrorMessage errorMessage: String!
         ) {
             print("onPhoneRetry due to")
-	    print(resultCode)
+            print(resultCode)
             self.v.cb = {
                 enterPhoneNumber.confirmText(self.v.$nbr.wrappedValue)
             }
@@ -203,48 +207,39 @@ struct ContentView: View {
     class CardLinkController: NSObject, CardLinkControllerCallback {
         func onAuthenticationCompletion(p0: ActivationResultProtocol?, cardLinkProtocols: Set<AnyHashable>) {
             print("onAuthComp")
-            if(p0?.getCode() == ActivationResultCode.OK) {
+            if p0?.getCode() == ActivationResultCode.OK {
                 let p = cardLinkProtocols.first
                 let p1 = p as! any PrescriptionProtocol
 
-		//Available prescriptions can be requested via the cardlinkProtocol instance
-		//one can either create a request object like in the following line
-		//let req = RequestPrescriptionList(iccsns: [KotlinByteArray(size: 0)], messageId: RandomUUID_iosKt.randomUUID())
-		//where an empty list means all available prescriptions for all cards in the session or is filtered by the given list
-		//and provide this to the requestPrescriptions() function
+                //Available prescriptions can be requested via the cardlinkProtocol instance
+                //one can either create a request object like in the following line
+                //let req = RequestPrescriptionList(iccsns: [KotlinByteArray(size: 0)], messageId: RandomUUID_iosKt.randomUUID())
+                //where an empty list means all available prescriptions for all cards in the session or is filtered by the given list
+                //and provide this to the requestPrescriptions() function
 
-		//one can also provide the iccsns for which the prescriptions should be requested provide directly as a list of strings as shown below. 
-		//available iccsns can be gathered after each card registration via the ActivationResultProtocol in the resultParameters
+                //one can also provide the iccsns for which the prescriptions should be requested provide directly as a list of strings as shown below.
+                //available iccsns can be gathered after each card registration via the ActivationResultProtocol in the resultParameters
 
                 DispatchQueue.main.sync {
-                    p1.requestPrescriptions(iccsns: ["80276883110000156308"], messageId: RandomUUID_iosKt.randomUUID()) { response,er in
-			print(response)
-                        //if let iccsn = response?.availablePrescriptionLists.first?.iccsn {
-                        //    let selectAll = SelectedPrescriptionList(
-                        //        iccsn: iccsn,
-                        //        prescriptionIndexList: [""],
-                        //        version: nil,
-                        //        supplyOptionsType: SupplyOptionsType.delivery,
-                        //        name: nil,
-                        //        address: nil,
-                        //        hint: nil,
-                        //        text: nil,
-                        //        phone: nil,
-                        //        mail: nil,
-                        //        messageId: "id"
-                        //    )
-                        //    DispatchQueue.main.sync {
-                        //        p1.selectPrescriptions(selection: selectAll) { responseSel, err in
-                        //            print(responseSel!)
-                        //        }
-                        //    }
-                        //}
+                    p1.requestPrescriptions(iccsns: [], messageId: RandomUUID_iosKt.randomUUID()) {
+                        response, er in
+                        if let f = (er as? NSError)?.toGenericErrorMessage() {
+                            switch f.errorCode {
+                            case GenericErrorResultType.noPrescriptionsAvailable:
+                                print(f.errorMessage)
+                            case GenericErrorResultType.unknownError:
+                                print(f.errorMessage)
+                            default:
+                                print("no error type")
+                            }
+                        } else {
+                            print(response)
+                        }
                     }
                 }
             } else {
                 print("process ended with an error.")
             }
-
 
         }
 
@@ -262,19 +257,17 @@ struct ContentView: View {
         let cardLinkInteraction = CardLinkInteraction(v: self)
         let url = "https://service.dev.epotheke.com/cardlink"
         //if environment allows unauthenticated access tenantToken can be null
-        let tenantToken : String? = nil
-        let sdk = SdkCore(cardLinkUrl: url,
-                      tenantToken: tenantToken,
-                      cardLinkControllerCallback: cardLinkController,
-                      cardLinkInteractionProtocol: cardLinkInteraction,
-                      sdkErrorHandler: sdkErrorHandler,
-                      nfcOpts: IOSNFCOptions())
+        let tenantToken: String? = nil
+        let sdk = SdkCore(
+            cardLinkUrl: url,
+            tenantToken: tenantToken,
+            cardLinkControllerCallback: cardLinkController,
+            cardLinkInteractionProtocol: cardLinkInteraction,
+            sdkErrorHandler: sdkErrorHandler,
+            nfcOpts: IOSNFCOptions())
         sdk.setDebugLogLevel()
         sdk.doInitCardLink()
-
     }
-
-
 }
 
 #Preview {
