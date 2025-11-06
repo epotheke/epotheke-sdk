2fee2b8471b788214e7c9670bd9b24b62376c23a
Florian Otto <florian.otto@ecsec.de>
2025-11-06 12:55:37 +0100

    fix: Add throws declaration to salHelper function

--- sdk/src/commonMain/kotlin/com/epotheke/cardlink/SmartcardSalHelper.kt
+++ sdk/src/commonMain/kotlin/com/epotheke/cardlink/SmartcardSalHelper.kt
@@ -1,16 +1,26 @@
 package com.epotheke.cardlink
 
+import io.ktor.utils.io.CancellationException
 import org.openecard.sal.sc.SmartcardDeviceConnection
 import org.openecard.sal.sc.SmartcardSalSession
 
 object SmartcardSalHelper {
-    suspend fun connectFirstTerminalOnInsertCard(salSession: SmartcardSalSession): SmartcardDeviceConnection {
-        val terminal =
-            salSession.sal.terminals
-                .list()
-                .firstOrNull() ?: throw OtherNfcError("NFC stack could not be initialized")
+    @Throws(
+        CardLinkClientError::class,
+        CancellationException::class,
+    )
+    suspend fun connectFirstTerminalOnInsertCard(salSession: SmartcardSalSession): SmartcardDeviceConnection =
+        try {
+            val terminal =
+                salSession.sal.terminals
+                    .list()
+                    .firstOrNull() ?: throw OtherNfcError("NFC stack could not be initialized")
 
-        terminal.waitForCardPresent()
-        return salSession.connect(terminal.name)
-    }
+            terminal.waitForCardPresent()
+            salSession.connect(terminal.name)
+        } catch (e: Exception) {
+            throw OtherNfcError(
+                cause = e,
+            )
+        }
 }
